f76fb44a099f1c6c140d6d4b3f2a938cc6d5f933
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2021-11-21 03:33:59 +0800

    culprit can now move

--- Assets/Scripts/Chapter1-3 scripts/Culprit.cs
+++ Assets/Scripts/Chapter1-3 scripts/Culprit.cs
@@ -7,23 +7,64 @@ public class Culprit : MonoBehaviour
     [SerializeField] Arch3Node source;
     [SerializeField] Arch3Node goal;
     [SerializeField] Arch3Edge path;
-    [SerializeField] int stepsTaken;
-    // [SerializedField] Slider maybeslider;
+
+    private int stepsTaken;
+    private int stepsToTake;
+    private bool shouldMove = false;
+    private float lerpPercent = 0.0f;
+    private float endVal;
 
     // Start is called before the first frame update
     void Start()
     {
+        reset();
+    }
+
+    void reset()
+    {
         stepsTaken = 0;
+        stepsToTake = 0;
+        shouldMove = false;
+        lerpPercent = 0.0f;
+        endVal = (float)path.GetWeight();
     }
 
     public void move(int steps)
     {
-        
+        if (!hasEscaped())
+        {
+            stepsToTake += steps;
+            shouldMove = true;
+        }
     }
 
-    // Update is called once per frame
+    // Update is called once per frame  
     void Update()
     {
-        
+        if (shouldMove)
+        {
+            // move position
+            lerpPercent += 0.01f;
+            transform.position = Vector2.Lerp(source.gameObject.transform.position, goal.gameObject.transform.position, lerpPercent);
+
+            // check if cur percent is whole number
+            float temp = endVal * lerpPercent;
+            if (temp >= stepsTaken + 1)
+            {
+                stepsToTake--;
+                stepsTaken++;
+            }
+
+             //stop if reached distance
+            if (stepsToTake == 0 || hasEscaped())
+            {
+                shouldMove = false;
+            }
+        }
     }
+
+    public bool hasEscaped()
+    {
+        return stepsTaken == endVal;
+    } 
 }
