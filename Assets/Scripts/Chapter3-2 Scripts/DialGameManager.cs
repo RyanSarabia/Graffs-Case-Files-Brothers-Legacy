4ae303d9423463049cf15bd292181d59d8b498fb
NOCTRL\noctr <mark_cai@dlsu.edu.ph>
2021-12-10 23:16:25 +0800

    ch3-3 basic mechanics done

---
+++ Assets/Scripts/Chapter3-2 Scripts/DialGameManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialGameManager : MonoBehaviour
+{
+    private static DialGameManager instance;
+    public static DialGameManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            GameObject.Destroy(gameObject);
+    }
+
+    [SerializeField] private List<GameObject> dialLights = new List<GameObject>();
+    private int nLights;
+    [SerializeField] private List<int> targetLights = new List<int>();
+
+    private int currLight = 0;
+
+    [SerializeField] private GameObject victoryCard;    
+    [SerializeField] private GameObject clickBlocker;
+    private bool panelFocus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nLights = dialLights.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void lightUpN(int num)
+    {
+        currLight = (currLight + num) % nLights;
+
+        if (!dialLights[currLight].activeSelf)
+        {
+            dialLights[currLight].SetActive(true);
+        }
+        else
+        {
+            dialLights[currLight].SetActive(false);
+        }
+            
+        checkFinal();
+    }
+
+    public void checkFinal()
+    {
+        int targetCount = targetLights.Count;
+        int count = 0;        
+        
+        foreach(var num in targetLights)
+        {
+            if (dialLights[num].activeSelf)
+            {
+                count++;
+            }
+        }
+
+        if(count == targetCount)
+        {
+            int litUp = 0;
+
+            foreach (var light in dialLights)
+            {
+                if (light.activeSelf)
+                {
+                    litUp++;
+                }
+            }
+
+            if (litUp == targetCount)
+            {
+                victoryCard.SetActive(true);
+                panelFocus = true;
+                clickBlocker.gameObject.SetActive(true);
+            }
+        }
+    }
+}
