96e05010a64726ace6a09d08326dbfe5796478c6
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2022-01-22 19:37:12 +0800

    added passing of data (location and desc) to modal

--- Assets/Scripts/Overworld Scripts/OverlayCanvas.cs
+++ Assets/Scripts/Overworld Scripts/OverlayCanvas.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class OverlayCanvas : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI locName;
+    [SerializeField] private TextMeshProUGUI desc;
+    [SerializeField] private Button goBtn;
+
     // Start is called before the first frame update
     void Start()
     {
         this.gameObject.SetActive(false);
+        this.goBtn.onClick.AddListener(handleBtnClick);
 
-        EventBroadcaster.Instance.AddObserver(GraphGameEventNames.OVERWORLD_NODE_CLICKED, openModal);
+        EventBroadcaster.Instance.AddObserver(GraphGameEventNames.OVERWORLD_NODE_CLICKED, setModalContent);
     }
 
-    public void openModal(Parameters par)
+    public void setModalContent(Parameters par)
     {
-        //this.gameObject.SetActive(true);
+        locName.SetText("Chapter " + par.GetStringExtra(OverworldIcons.LOC_NAME, "X-X: Location"));
+        desc.SetText(par.GetStringExtra(OverworldIcons.DESC, "description not found"));
+
+        //set button params here
+    }
+
+    private void handleBtnClick()
+    {
+        //set button params in setModalContent()
     }
 
     // Update is called once per frame
