8a045560ab02d0ff0e6b05333423677df35b587a
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2022-01-30 18:20:49 +0800

    added options overlay canvas

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    private SceneLoader sceneLoader;
+    [SerializeField] private AudioSource soundManager;
+    [SerializeField] private Button overworldBtn;
+    [SerializeField] private Button titleScreenBtn;
+    [SerializeField] private Slider volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = soundManager.volume;
+        
+        sceneLoader = SceneLoader.GetInstance();
+        volumeSlider.onValueChanged.AddListener(delegate { changeVolume(); } );
+        overworldBtn.onClick.AddListener(delegate { sceneLoader.returnToOverworld(); } );
+        titleScreenBtn.onClick.AddListener(delegate { sceneLoader.loadSceneString("TitleScreen"); } );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void changeVolume()
+    {
+        soundManager.volume = volumeSlider.value;
+    }
+}
