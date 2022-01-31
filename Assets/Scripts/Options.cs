fcc31519058baa3172cf5183d7c2281abb717179
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2022-02-01 01:09:24 +0800

    edited optionsCanvas, 1-1, and overworld

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -6,22 +6,29 @@ using UnityEngine.UI;
 public class Options : MonoBehaviour
 {
     private SceneLoader sceneLoader;
-    [SerializeField] private AudioSource soundManager;
+    [SerializeField] private AudioSource SFXManager;
+    [SerializeField] private AudioSource musicManager;
     [SerializeField] private Button overworldBtn;
     [SerializeField] private Button titleScreenBtn;
-    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Slider SFXVolumeSlider;
+    [SerializeField] private Slider musicVolumeSlider;
     [SerializeField] private Toggle dontRemind;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (soundManager)
+        if (SFXManager)
         {
-            volumeSlider.value = soundManager.volume;
+            SFXVolumeSlider.value = SFXManager.volume;
         }
-        
+        if (musicManager)
+        {
+            musicVolumeSlider.value = musicManager.volume;
+        }
+
         sceneLoader = SceneLoader.GetInstance();
-        volumeSlider.onValueChanged.AddListener(delegate { changeVolume(); } );
+        SFXVolumeSlider.onValueChanged.AddListener(delegate { changeVolume(SFXManager, SFXVolumeSlider); });
+        musicVolumeSlider.onValueChanged.AddListener(delegate { changeVolume(musicManager, musicVolumeSlider); });
         overworldBtn.onClick.AddListener(delegate { sceneLoader.returnToOverworld(); } );
         titleScreenBtn.onClick.AddListener(delegate { sceneLoader.loadSceneString("TitleScreen"); } );
     }
@@ -32,8 +39,11 @@ public class Options : MonoBehaviour
         
     }
 
-    private void changeVolume()
+    private void changeVolume(AudioSource source, Slider slider)
     {
-        soundManager.volume = volumeSlider.value;
+        if (source)
+        {
+            source.volume = slider.value;
+        }
     }
 }
