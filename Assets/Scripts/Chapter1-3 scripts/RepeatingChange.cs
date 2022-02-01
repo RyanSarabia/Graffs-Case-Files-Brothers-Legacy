5387730d5f1cf85dbcdd04d4b295af20acf841b1
NOCTRL\noctr <mark_cai@dlsu.edu.ph>
2022-02-02 02:08:25 +0800

    2-3 2nd phase now changes color

--- Assets/Scripts/Chapter1-3 scripts/RepeatingChange.cs
+++ Assets/Scripts/Chapter1-3 scripts/RepeatingChange.cs
@@ -23,11 +23,21 @@ public class RepeatingChange : MonoBehaviour
     [SerializeField] private int evenChange;
     [SerializeField] private List<Arch3Edge> oddEdges = new List<Arch3Edge>();
     [SerializeField] private List<Arch3Edge> evenEdges = new List<Arch3Edge>();
-    
+
+    [SerializeField] private bool ch2;
+    [SerializeField] private GameObject color1;
+    [SerializeField] private GameObject color2;
+    [SerializeField] private List<GameObject> firstHalf = new List<GameObject>();
+    [SerializeField] private List<GameObject> secondHalf = new List<GameObject>();
+    private Color c1;
+    private Color c2;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        c1 = color1.GetComponent<SpriteRenderer>().color;
+        c2 = color2.GetComponent<SpriteRenderer>().color;
     }
 
     // Update is called once per frame
@@ -54,6 +64,27 @@ public class RepeatingChange : MonoBehaviour
                 else
                     edge.addWeight(oddChange);
             }
+
+            if (ch2)
+                colorSwap(turn);
+        }
+    }
+
+    public void colorSwap(int turn)
+    {
+        foreach (var edge in firstHalf)
+        {
+            if (turn % 2 == 0)
+                edge.GetComponent<SpriteRenderer>().color = c1;
+            else
+                edge.GetComponent<SpriteRenderer>().color = c2;
+        }
+        foreach (var edge in secondHalf)
+        {
+            if (turn % 2 == 0)
+                edge.GetComponent<SpriteRenderer>().color = c2;
+            else
+                edge.GetComponent<SpriteRenderer>().color = c1;
         }
     }
 }
