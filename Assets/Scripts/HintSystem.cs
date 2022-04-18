35d33645faa1287ad262c8c9a070318974ae9a94
BrianJSo <51768646+BrianJSo@users.noreply.github.com>
2022-04-18 15:03:40 +0800

    bugged hint system
    hint level not resetting

--- Assets/Scripts/HintSystem.cs
+++ Assets/Scripts/HintSystem.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
 
 public class HintSystem : MonoBehaviour
 {
@@ -13,7 +15,23 @@ public class HintSystem : MonoBehaviour
     }
 
     [SerializeField] private int hintLvl;
-    private string hintString;    
+    [SerializeField] private Button hintBtn;
+    [SerializeField] private Sprite hintBtnDefault;
+    [SerializeField] private Sprite hintBtnAlt;
+    [SerializeField] private Image img;
+    [SerializeField] private TextMeshProUGUI textBox;
+    [SerializeField] private GameObject prevBtn;
+    [SerializeField] private GameObject nextBtn;
+    [SerializeField] private GameObject doneBtn;
+
+    [SerializeField] private List<Sprite> pics = new List<Sprite>();
+    [SerializeField] private List<string> texts = new List<string>();
+
+    private bool newHint = false;
+    private string hintString;
+    private string newHintString;
+
+    private int curIdx = 0; 
 
     private void Awake()
     {
@@ -23,13 +41,20 @@ public class HintSystem : MonoBehaviour
             GameObject.Destroy(gameObject);
 
         hintString = SceneManager.GetActiveScene().name + "hints";
+        newHintString = SceneManager.GetActiveScene().name + "newHint";
 
         hintLvl = PlayerPrefs.GetInt(hintString);
+        int newHintInt = PlayerPrefs.GetInt(newHintString);
+        newHint = newHintInt == 1 ? true : false;
     }    
 
     // Start is called before the first frame update
     void Start()
-    {       
+    {
+        if (newHint)
+        {
+            triggerNewHint();
+        }
     }
 
     // Update is called once per frame
@@ -38,9 +63,31 @@ public class HintSystem : MonoBehaviour
         
     }
 
+    private void triggerNewHint()
+    {
+        if(hintLvl == 1)
+        {
+            hintBtn.gameObject.SetActive(true);
+        }
+        hintBtn.GetComponent<Image>().sprite = hintBtnAlt;
+
+    }
+
+    public void newHintOpened()
+    {
+        PlayerPrefs.SetInt(newHintString, 0);
+        newHint = false;
+        hintBtn.GetComponent<Image>().sprite = hintBtnDefault;
+    }
+
     public void hintUp()
     {
+        Debug.Log("Hint Level: "+ hintLvl);
         PlayerPrefs.SetInt(hintString, PlayerPrefs.GetInt(hintString) + 1);
+        if (PlayerPrefs.GetInt(hintString) + 1 <= 3)
+        {
+            PlayerPrefs.SetInt(newHintString, 1);
+        }
     }
 
     public int getHintLvl()
@@ -50,5 +97,51 @@ public class HintSystem : MonoBehaviour
     public void resetHintLvl()
     {
         PlayerPrefs.SetInt(hintString, 0);
+    }
+
+
+    private void changeContent(int idx)
+    {
+        if (pics.Count > 0 && texts.Count > 0)
+        {
+            img.sprite = pics[idx];
+            textBox.text = texts[idx];
+
+            if (idx == hintLvl -1)
+            {
+                nextBtn.SetActive(false);
+                doneBtn.SetActive(true);
+            }
+            else
+            {
+                nextBtn.SetActive(true);
+                doneBtn.SetActive(false);
+            }
+
+            if (idx == 0)
+            {
+                prevBtn.SetActive(false);
+            }
+            else
+            {
+                prevBtn.SetActive(true);
+            }
+        }
+    }
+
+    public void next()
+    {
+        curIdx++;
+        changeContent(curIdx);
+    }
+    public void prev()
+    {
+        curIdx--;
+        changeContent(curIdx);
+    }
+    public void done()
+    {
+        curIdx = 0;
+        changeContent(curIdx);
     }
 }
--- Assets/Scripts/HintSystemEditor.cs
+++ Assets/Scripts/HintSystemEditor.cs
@@ -6,9 +6,50 @@ using UnityEditor;
 [CustomEditor(typeof(HintSystem))]
 public class HintSystemEditor : Editor
 {
-    // Start is called before the first frame update
+    SerializedProperty hintLvl;
+    SerializedProperty hintBtn;
+    SerializedProperty hintBtnDefault;
+    SerializedProperty hintBtnAlt;
+    SerializedProperty img;
+    SerializedProperty textBox;
+    SerializedProperty prevBtn;
+    SerializedProperty nextBtn;
+    SerializedProperty doneBtn;
+    SerializedProperty pics;
+    SerializedProperty texts;
+
+    void OnEnable()
+    {
+        hintLvl = serializedObject.FindProperty("hintLvl");
+        hintBtn = serializedObject.FindProperty("hintBtn");
+        hintBtnDefault = serializedObject.FindProperty("hintBtnDefault");
+        hintBtnAlt = serializedObject.FindProperty("hintBtnAlt");
+        img = serializedObject.FindProperty("img");
+        textBox = serializedObject.FindProperty("textBox");
+        prevBtn = serializedObject.FindProperty("prevBtn");
+        nextBtn = serializedObject.FindProperty("nextBtn");
+        doneBtn = serializedObject.FindProperty("doneBtn");
+        pics = serializedObject.FindProperty("pics");
+        texts = serializedObject.FindProperty("texts");
+    }
+
     public override void OnInspectorGUI()
     {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(hintLvl);
+        EditorGUILayout.PropertyField(hintBtn);
+        EditorGUILayout.PropertyField(hintBtnDefault);
+        EditorGUILayout.PropertyField(hintBtnAlt);
+        EditorGUILayout.PropertyField(img);
+        EditorGUILayout.PropertyField(textBox);
+        EditorGUILayout.PropertyField(prevBtn);
+        EditorGUILayout.PropertyField(nextBtn);
+        EditorGUILayout.PropertyField(doneBtn);
+        EditorGUILayout.PropertyField(pics);
+        EditorGUILayout.PropertyField(texts);
+        serializedObject.ApplyModifiedProperties();
+
+
         if (EditorApplication.isPlaying)
             DrawDefaultInspector();
 
